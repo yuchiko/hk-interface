bf038c11fe5da76f0dbd49830e3596ba77498979
yuchiko <iyuchiko@gmail.com>
2020-06-05 00:46:48 +0300

    added scoped styles, added scss support

--- src/components/button/Button.vue
+++ src/components/button/Button.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    I AM a button!!
+  <div class="button">
+    <span class="ddd">
+      I AM a button!!
+    </span>
   </div>
 </template>
+
 <script>
-export default {
-  name: 'HkButton',
-}
+  export default {
+    name: 'HkButton',
+  }
 </script>
+
+<style lang="scss" scoped>
+  .ddd {
+    color:red!important;
+  }
+</style>
